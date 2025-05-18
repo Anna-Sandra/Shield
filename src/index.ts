8a1a201e2c53db43824b867e48c8d5bff77ac31f
Anna-Sandra <sandraoduro025@gmail.com>
2025-05-18 22:17:31 +0000

    Updated TypeScript server logic

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,51 @@ import path from "path";
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-      res.end('Hello, World!\n'); 
+    if (req.url === '/') {
+        const filePath = path.join(__dirname, 'index.html');
+        
+        fs.readFile(filePath, (err, content) => {
+          if (err) {
+            res.writeHead(500);
+            res.end('Error loading HTML file');
+          } else {
+            //res.writeHead(200, { 'Content-Type': 'text/html' });
+
+            res.writeHead(200);
+            res.end(content);
+          }
+        });
+    }else if (req.url === '/style.css'){
+        const cssPath =path.join(__dirname, 'style.css');
+
+        fs.readFile(cssPath, (err, content) => {
+            if (err) {
+              res.writeHead(500);
+              res.end('Error loading CSS');
+            } else {
+              //res.writeHead(200, { 'Content-Type': 'text/css' });
+
+              res.writeHead(200);
+              res.end(content);     
+        }
+    });
+    } else if (req.url === '/index.jpg') {
+        const imgPath = path.join(__dirname, 'index.jpg');
+        fs.readFile(imgPath, (err, content) => {
+            if (err) {
+              res.writeHead(500);
+              res.end('Error loading image');
+            } else {
+              //res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+
+              res.writeHead(200);
+              res.end(content);
+      }
+    });
+      } else {
+        res.writeHead(404);
+        res.end('Page not found');
+      } 
   });
 
 server.listen(PORT, ()=>{
