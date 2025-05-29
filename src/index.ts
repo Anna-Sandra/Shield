9100f34634d98cbc7dc9c2daadc84464f5cda38d
Anna-Sandra <sandraoduro025@gmail.com>
2025-05-29 18:44:21 +0000

    week3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import http from "http";
 import fs from "fs";
 import path from "path";
+import express from "express";
+import bodyParser from "body-parser";
+import { Request, Response } from "express";
 
 const PORT = 3000;
+const app = express();
+
+app.use(express.json());
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
@@ -52,7 +58,48 @@ const server = http.createServer((req, res) => {
       } 
   });
 
-server.listen(PORT, ()=>{
+  let idCounter = 1;
+  let tasks: {id: number, title: string }[] =[];
+
+  app.post('/tasks', (req, res) => {
+    const { title } = req.body;
+    const task = {id: idCounter++, title },
+    tasks.push(task);
+    res.status(201).json(task);
+  });
+
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+
+app.put('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  const task = tasks.find(t => t.id === parseInt(id));
+
+  if (!task) return res.status(404).json({ error: 'Task not found' });
+
+  task.title = title;
+  res.json(task);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const taskIndex = tasks.findIndex(t => t.id === taskId);
+  
+  if (taskIndex === -1) {
+      return res.status(404).json({ error: 'Task not found' });
+  }
+  
+  const deletedTask = tasks.splice(taskIndex, 1);
+  res.json(deletedTask[0]);
+});
+
+
+
+app.listen(PORT, ()=>{
 console.log(`Server running at http://localhost:${PORT}`);
 })
 
