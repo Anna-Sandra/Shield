dceff29cd41417330c6e9f76560ec547a4689bcc
Anna-Sandra <sandraoduro025@gmail.com>
2025-05-31 02:44:18 +0000

    week3

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,19 @@ app.get("/", (req, res) => {
   res.send("shield is live");
 });
 
+app.post("/users", (req, res) => {
+  res.send("User created");
+});
+
+app.put("/users/:id", (req, res) => {
+  res.send(`User with ID ${req.params.id} updated`);
+});
+
+app.delete("/users/:id", (req, res) => {
+  res.send(`User with ID ${req.params.id} deleted`);
+});
+
 app.use("/users", userRouter);
+
 
 export default app;
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -14,8 +14,35 @@ export async function getUsers(req: Request, res: Response) {
   }
 }
 
-export async function createUser(req: Request, res: Response) {}
+export async function createUsers(req: Request, res: Response) {
+try{
 
-export async function updateUser(req: Request, res: Response) {}
+  const { name, email, password } = req.body;
 
-export async function deleteUser(req: Request, res: Response) {}
+  res.status(200).json({ message: "User created successfully.",});
+} catch (error) {
+  res.status(500).json({ message: "Error fetching users" });
+}
+}
+
+
+export async function updateUsers(req: Request, res: Response) {
+  try {
+    const userId = req.params.id; 
+    const { name, email } = req.body;
+
+    res.status(200).json({message: "User updated successfully.",});
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users" });
+  }
+}
+
+export async function deleteUsers(req: Request, res: Response) {
+try{
+  const userId = req.params.id;
+
+  res.status(200).json({message: "User deleted successfully.",});
+} catch (error) {
+  res.status(500).json({ message: "Error fetching users" });
+}
+}
